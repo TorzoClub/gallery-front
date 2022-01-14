f67a23c322bf5f04131cd7ac468c31f8ab66657d
vec <vec@torzo.club>
2022-01-14 12:49:21 +0800

    [fix] scp command

--- scripts/upload-development.sh
+++ scripts/upload-development.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
-scp -r build ${SSH_USERNAME}@${SSH_HOST}:${REMOTE_UPLOAD_PATH} -i ./tmp/private.pem
+scp -i ./tmp/private.pem -r build/* ${SSH_USERNAME}@${SSH_HOST}:${REMOTE_UPLOAD_PATH}
