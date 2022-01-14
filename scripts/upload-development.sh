d29b9b9c4ce090b1d1a91cd587edbf74ba72d610
vec <vec@torzo.club>
2022-01-14 13:16:12 +0800

    [adjust] ssh-key

--- scripts/upload-development.sh
+++ scripts/upload-development.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 
-scp -i ./tmp/private.pem -r build/* ${SSH_USERNAME}@${SSH_HOST}:${REMOTE_UPLOAD_PATH}
+scp -r build/* ${SSH_USERNAME}@${SSH_HOST}:${REMOTE_UPLOAD_PATH}
