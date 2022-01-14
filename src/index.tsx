d1475828af2a9ef32cc384a5d5bdbb8b7869ea01
vec <vec@torzo.club>
2022-01-14 09:06:21 +0800

    [fix] ESLint Warning

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
 import store from './store'
 
 // const VConsole = require('vconsole')
 //   window.vConsole = new VConsole()
 
-ReactDOM.render(<App store={ store } />, document.getElementById('root'));
+ReactDOM.render(<App store={ store } />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
