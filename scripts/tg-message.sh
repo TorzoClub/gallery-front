e68bb954154506bbad3623dda8298a9886883748
vec <vec@torzo.club>
2022-01-15 08:50:22 +0800

    [fix] TGBot use JSON Message

--- scripts/tg-message.sh
+++ scripts/tg-message.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-curl https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=${MESSAGE_TEXT}
+curl -v https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage \
+-H "Content-Type: application/json" \
+-d "{ \"chat_id\": \"${TELEGRAM_CHAT_ID}\", \"text\": \"${MESSAGE_TEXT}\" }"
