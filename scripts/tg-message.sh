33bf8ffef858ee8b9e366776cfe5a188a75df4e5
vec <vec@torzo.club>
2022-01-16 14:28:38 +0800

    [add] TGBot release message show comment list

--- scripts/tg-message.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -v https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage \
--H "Content-Type: application/json" \
--d "{ \"chat_id\": \"${TELEGRAM_CHAT_ID}\", \"text\": \"${MESSAGE_TEXT}\" }"
